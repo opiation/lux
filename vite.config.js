5f84ad925d5272cd48d4c63e6daa744654a8ffcc
Nicholas Gelinas <nick.gelinas@gmail.com>
2024-06-14 11:17:49 -0400

    ci: :construction_worker: add preliminary test coverage

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,9 @@ import react from "@vitejs/plugin-react-swc";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [autoInstall("bun"), react()],
+  test: {
+    coverage: {
+      enabled: true,
+    },
+  },
 });
