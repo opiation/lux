99e1b9316a99a7648398a4742cdc96e506e8478b
Nicholas Gelinas <nick.gelinas@gmail.com>
2024-06-14 01:02:06 -0400

    chore: export all schema model in a centralized way

---
+++ src/core/schema.ts
@@ -0,0 +1,5 @@
+export * from "./schema/organization.ts";
+export * from "./schema/person.ts";
+export * from "./schema/resource.ts";
+export * from "./schema/user.ts";
+export * from "./schema/uuid.js";
