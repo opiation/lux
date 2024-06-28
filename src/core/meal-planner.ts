c25d47fcf26f8e60eb3d968b06a7666c7f78759d
Nicholas Gelinas <nick.gelinas@gmail.com>
2024-06-28 14:09:02 -0400

    feat: extract Ollama API calls to its own client
    This is helpful for keeping the meal planning logic fairly focused and
    not full of Ollama specific settings and params. This also adds a
    little documentation and proper type checking to the whole API calling.

--- src/core/meal-planner.ts
+++ src/core/meal-planner.ts
@@ -1,59 +1,43 @@
-import { z } from "zod";
+import * as Ollama from "./ollama.js";
 
 export interface MealPlanner {
-  generateMealPlans(
-    params?: Partial<MealPlanner.MealPlanRequestParams>
-  ): Promise<string>;
+  /**
+   * Generate a meal plan with the given {@link MealPlanRequestParams} `params`.
+   */
+  generateMealPlans(params?: Partial<MealPlanRequestParams>): Promise<string>;
 }
 
-// eslint-disable-next-line @typescript-eslint/no-namespace
-export namespace MealPlanner {
-  export type MealPlanRequestParams = {
-    adultCount: number;
-    childCount: number;
-    mealCount: number;
-  };
+/**
+ * Acceptable parameters for generating a meal plan.
+ */
+export type MealPlanRequestParams = {
+  adultCount: number;
+  childCount: number;
+  mealCount: number;
 }
 
 export const defaultMealPlanRequestParams = {
   adultCount: 2,
   childCount: 2,
   mealCount: 7,
-} as const satisfies MealPlanner.MealPlanRequestParams;
+} as const satisfies MealPlanRequestParams;
 
-export function usingOllama(
-  baseAPIURL = "http://localhost:11434/api",
-  model = "llama3"
-) {
-  const responseBody = z.object({
-    model: z.string().trim().min(1),
-    response: z.string().trim().min(1),
-    done: z.boolean(),
-  });
-
+export function usingOllama(model = "llama3") {
   return {
     async generateMealPlans(params) {
       const { adultCount, childCount, mealCount } = params
         ? { ...defaultMealPlanRequestParams, ...params }
         : defaultMealPlanRequestParams;
-
-      const response = await fetch(`${baseAPIURL}/generate`, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          model,
-          prompt: `I need help planning dinner for the next ${mealCount} days. We are a family of ${adultCount + childCount} with ${adultCount} adults and ${childCount} young children. We follow a vegetarian diet preferring vegan options when available. Please provide ${mealCount} meal ideas including at least a main dish and side dish for each meal.`,
-          stream: false,
-          system:
+      
+      const completion = await Ollama.generate({
+        model,
+        prompt: `I need help planning dinner for the next ${mealCount} days. We are a family of ${adultCount + childCount} with ${adultCount} adults and ${childCount} young children. We follow a vegetarian diet preferring vegan options when available. Please provide ${mealCount} meal ideas including at least a main dish and side dish for each meal.`,
+        stream: false,
+        system:
             "You are a dietitian specializing in planning healthy meals meeting vegetarian and vegan dietary restrictions for adults and young children.",
-        }),
       });
 
-      const body = responseBody.parse(await response.json());
-
-      return body.response;
+      return completion.response;
     },
   } satisfies MealPlanner;
 }
